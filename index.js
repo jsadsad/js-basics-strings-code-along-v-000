6189b55a4f960508d876703f39e5858d675074db
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-28 23:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
+const firstInitial = currentUser.slice(0,1)
+
+const shortGreeting = `Welcome ${
